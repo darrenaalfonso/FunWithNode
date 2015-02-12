6bc6acd0638242d8613046d86764e3c52138161b
Darren Alfonso <dalfonso@outlook.com>
2015-02-12 12:31:25 -0500

    added packages

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require("http");
+var express = require("express");
+var app = express();
 
-var server = http.createServer(function(req, res){
-	console.log(req.url);
-	res.write("<html><body><h1>" + req.url + "</h1></body></html>");
-	res.end();
-});
+app.get("/", function(req, res){
+	res.send("<html><body><h1>Express</h1></body></html>");
+})
+
+var server = http.createServer(app);
 
 server.listen(3000);
